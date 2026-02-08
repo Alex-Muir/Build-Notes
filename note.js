bf83af4aa819fb9917ea45999e4e04b470e18db6
Alex Muir <wamuir1212@protonmail.com>
2026-02-07 18:00:10 -0800

    Added a updateNote function and a filterByTag function. Added function calls in main to test this functionality. Data does not persist at this point.

--- note.js
+++ note.js
@@ -2,7 +2,7 @@
 
 // Create a new note from user input (title, content, and tags)
 async function createNote(rl) {
-    const title = (await rl.question("What is the note's title? ")).trim();
+    const title = (await rl.question("\nWhat is the note's title? ")).trim();
     const content = (await rl.question("Please write your note: ")).trim();
     const tags = []
     while (true) {
@@ -10,20 +10,50 @@ async function createNote(rl) {
         if (!tag) break;
         tags.push(tag);
     } 
-
+    
     return {
         id: Date.now(), 
         title: title, 
         content: content, 
         tags: tags, 
-        createdAt: new Date()
+        createdAt: new Date(), 
+        editedAt: null
     };
-
 }
 
 // Remove a note
 function removeNote(index, notes) {
     notes.splice(index, 1);
+}
+
+// Update (Edit) a note
+async function updateNote(rl, index, notes) {
+    console.log(`\nThis is the note to edit: ${notes[index].content}`)
+    const newContent = (await rl.question("What would you like to add? ")).trim();
+    notes[index].content += " " + newContent;
+    notes[index].editedAt = new Date();
+}
+
+// Filter notes by tag
+async function filterByTag(rl, notes) {
+    const searchTags = [];
+    const notesByTag = [];
+    while(true) {
+        let tag = (await rl.question("\nWhat tag(s) would you like to search for? ")).trim();
+        if(!tag) break;
+        searchTags.push(tag);
+    }
+
+    for (let i = 0; i < notes.length; i++) {
+        for (let j = 0; j < searchTags.length; j++) {
+            if(notes[i].tags.includes(searchTags[j])) {
+                notesByTag.push(notes[i]);
+                break
+            }
+        }
+    }
+
+    return notesByTag;
 }
 
 async function main() {
@@ -36,13 +66,26 @@ async function main() {
         output: process.stdout
         });
 
-    note = await createNote(rl);
-    notes.push(note)
-    rl.close();
-    console.log(notes);
-    removeNote(0, notes);
-    console.log(notes);
+    // Push several notes and print notes
+    notes.push(await createNote(rl));
+    notes.push(await createNote(rl));
+    notes.push(await createNote(rl));
+    console.log("\nNotes: ", notes);
 
+    // Update a notes content and print notes
+    await updateNote(rl, 1, notes)
+    console.log("\nUpdated Notes: ", notes);
+
+    // Filter notes by tag and print filtered notes
+    const filteredNotes = await filterByTag(rl, notes);
+    console.log("\nFiltered Notes: ", filteredNotes);
+
+    // Close user input interface
+    rl.close();
+
+    // Remove a note and print notes
+    removeNote(0, notes);
+    console.log("\nRemoved Notes: ", notes);
 }
 
 main();
