dd8ccd626a2678a8b7006e9049d2964fee5ee0e2
Alex Muir <wamuir1212@protonmail.com>
2026-04-10 13:02:47 -0700

    Edited PreviousNotes.jsx so that it returns list elements for the notes that have been created.

--- src/components/PreviousNotesSection.jsx
+++ src/components/PreviousNotesSection.jsx
@@ -1,7 +1,13 @@
-export default function PreviousNotesSection() {
+export default function PreviousNotesSection( {listItems, handleClick}) {
+    
+    const listOfNotes = listItems.map(item => 
+        <li key={item.id} onClick={() => {handleClick(item.id)}}>{item.title}</li>
+    )
+
     return (
         <div className="PreviousNotesSection">
             <h2 className="SectionName">Previous Notes</h2>
+            <ol>{listOfNotes}</ol>
         </div>
     )
 }
