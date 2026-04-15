afd1b3425e4f79417c182d593962858011e67ff9
Alex Muir <wamuir1212@protonmail.com>
2026-04-14 22:33:21 -0700

    Added EditNote.jsx for the Edit Note page. The structure is very similar to the NoteSection.jsx file. The differences are that EditNote has a note prop that uses data from the currentNote, it has a handlers prop which is an array of functions, and each Input has a pre-filled defaultValue from the currentNote.

---
+++ src/components/EditNote.jsx
@@ -0,0 +1,46 @@
+import Button from "./Button";
+import Input from "./Input";
+
+export default function EditNote( {editModeOn, handlers, note} ) {
+    if(!editModeOn) {
+        return null;
+    }
+
+    const[handleEditNote, cancelEditNote] = handlers;    
+    // !! Need to create an edit submit function !!
+    return (
+        <div className="EditNoteSection">
+            <h2 className="SectionName">New Note</h2>
+            <form action={handleEditNote} className="NoteForm">
+                <Input 
+                    labelFor="title"
+                    label="Title:" 
+                    type="text" 
+                    name="title" 
+                    required
+                    defaultValue={note.title}>
+                </Input>
+                <Input 
+                    labelFor="content" 
+                    label="Note Content:" 
+                    name="content" 
+                    multiline 
+                    required
+                    defaultValue={note.content}>
+                </Input>
+                <Input
+                    labelFor="tags"
+                    label="Tags (separate by comma)"
+                    type="text"
+                    name="tags"
+                    placeholder="tag1,tag2,tag3"
+                    defaultValue={note.tags}>
+                </Input>
+                <div>
+                    <Button type="submit">Save Changes</Button>
+                    <Button type="button" handleClick={cancelEditNote}>Cancel</Button>
+                </div>
+            </form>
+        </div>
+    );
+}
