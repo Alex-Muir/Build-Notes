a42a81bc60e228a24d3e00bba2d99a9159e8214e
Alex Muir <wamuir1212@protonmail.com>
2026-04-16 18:08:20 -0700

    Passed two props to SearchSection. listItems, which is the filtered list of notes based on the search query, and handlers, which is an array of functions necessary for full functionality of the search section. Rendered an array of list elements based on listItems to be displayed in the Search Section

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,21 +1,25 @@
 import Button from "./Button";
 import Input from "./Input";
 
-export default function SearchSection() {
+export default function SearchSection({ listItems, handlers }) {
+
+    const [handleSearch, handleNoteClick, clearSearch] = handlers;
+
+    const listOfResults = listItems.map(item => 
+        <li key={item.id} onClick={() => {handleNoteClick(item.id)}}>{item.title}</li>
+    )
+
     return (
         <div className="SearchSection">
             <h2 className="SectionName">Search Notes</h2>
-            <form className="SearchForm">
-                <Input 
-                    labelFor="search" 
-                    label="Search (Separate by comma)" 
-                    type="search" 
-                    name="search" 
-                    placeholder="query1, query2, query3" 
-                    required>
-                </Input>
-                <Button type="button">Search</Button>
-            </form>
+            <Input  
+                label="Search (Separate by comma)" 
+                type="search"  
+                placeholder="query1, query2, query3"
+                onChange={(e) => handleSearch(e.target.value)}>
+            </Input>
+            <Button type="button" handleClick={clearSearch}>Clear</Button>
+            <ul>{listOfResults}</ul>
         </div>
     );
 }
